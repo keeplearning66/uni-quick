67dcaa88b6072b346a397fe42f90257b5fec08dd
keeplearning66 <1256734885@qq.com>
2026-01-25 20:54:01 +0800

    chore: tweak

--- config/common/visualizer.ts
+++ config/common/visualizer.ts
@@ -3,7 +3,6 @@ import { visualizer } from 'rollup-plugin-visualizer';
 export function createVisualizer() {
   return visualizer({
     emitFile: false,
-    filename: 'stats.html',
     open: true,
   });
 };
